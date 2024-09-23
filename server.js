32a97388c1367c0e3e3467c2b7aec882b2c7d49f
oumayma <oumayma.bennamoun@outlook.com>
2024-09-23 07:21:08 -0400

    page adjustments

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ app.get("/", (req, res) => {
     });
 });
 
-app.get("/projects", (req,res) => {
-    fs.readFile(path.join(__dirname, "views", "projects.html"), "utf8", (err, content) => {
+app.get("/", (req,res) => {
+    ejs.renderFile(path.join(__dirname, "views", "partials", "footer.ejs"), "utf8", (err, content) => {
         if (err) return res.status(500).send("Error loading projects page");
         res.send(res.locals.header + content + res.locals.footer);
     });
