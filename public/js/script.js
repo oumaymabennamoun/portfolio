e2b87bfd12469e7188701a0039f21ad57e3b66f7
oumayma <oumayma.bennamoun@outlook.com>
2024-10-14 22:53:25 -0400

    more theme revisions

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,7 @@
 window.addEventListener("scroll", () => {
     const header = document.getElementsByTagName("header")[0];
 
-    if (window.scrollY > 10) {
+    if (window.scrollY > 15) {
         header.classList.add("scroll");
     } else {
         header.classList.remove("scroll");
