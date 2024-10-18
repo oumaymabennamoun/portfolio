d2c704f55daf76122aa87127d25cc610d1b72e3e
oumayma <oumayma.bennamoun@outlook.com>
2024-10-17 22:44:22 -0400

    filter tags function

--- public/js/script.js
+++ public/js/script.js
@@ -7,3 +7,27 @@ window.addEventListener("scroll", () => {
         header.classList.remove("scroll");
     }
 });
+
+const getQueryParameter = (param) => {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+
+}
+
+const tag = getQueryParameter("tag");
+
+if (tag) {
+    document.querySelectorAll("div[filter-tags]").forEach(item => {
+        const tags = item.getAttribute("filter-tags").split(",");
+
+        if (tags.includes(tag)) {
+            item.style.display = "block"; 
+        } else {
+            item.style.display = "none"; 
+        }
+    });
+} else {
+    document.querySelectorAll("div[filter-tags]").forEach(item => {
+        item.style.display = "block";
+    });
+}
